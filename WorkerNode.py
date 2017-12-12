902b1dde510deadd50a343088b0c8285d5e4b197
kaushal0 <kaushalgadhia@gmail.com>
2017-12-12 15:14:09 +0000

    Fetching Commits

--- WorkerNode.py
+++ WorkerNode.py
@@ -18,3 +18,15 @@ if __name__ == '__main__':
     print("Initialization Complete..")
 
     commits = requests.get(repoURL,json = {'pullState' : True})         #Notify managerNode for success
+
+    while True:
+        cyclo = requests.get(cycloURL)
+        print("Got Cyclo")
+        print(cyclo.)
+        json_data = json.loads(cyclo.text)
+        print(json_data)
+        print("Received: ",json_data['sha'])
+        if json_data['sha'] == -2:       # Polling for manager to give commits
+            print("Polling")
+        else:
+            subprocess.call(["bash", "getCommit.sh", json_data['sha']])
---
+++ getCommit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd Data
+
+git reset --hard $1
