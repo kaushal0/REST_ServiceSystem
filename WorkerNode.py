8260c51d70278ebe601f5955660d1062d25b1ae7
kaushal0 <kaushalgadhia@gmail.com>
2017-12-13 02:54:17 +0000

    computing

--- WorkerNode.py
+++ WorkerNode.py
@@ -28,4 +28,21 @@ if __name__ == '__main__':
         if json_data['sha'] == -2:       # Polling for manager to give commits
             print("Polling")
         else:
+            if json_data['sha'] == -1:
+                print("END")
+                break
             subprocess.call(["bash", "getCommit.sh", json_data['sha']])
+        CCValue = subprocess.check_output(["radon", "cc", "-s", "-a" , "Data"])
+        radonCCValue = CCValue.decode("utf-8")
+
+        print(radonCCValue)
+
+        avgCC = radonCCValue.rfind("(")
+        if radonCCValue[avgCC+1:-2] == "":
+            print("No Files")
+            r = requests.post(cycloURL, json={'commits': json_data['sha'], 'complexity': -1})
+        else:
+            averageCC = float(radonCCValue[avgCC+1:-2])  #Average cyclomatic complexity
+            r = requests.post(cycloURL, json={'commits': json_data['sha'], 'complexity': averageCC})
+        commitsDone += 1  # Increment the number of commits this node has completed
+print("Commits Done: ", commitsDone)
